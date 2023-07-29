64c545117cb20174aa20134d17c46caf617fdd9b
QuakeWang <1677980708@qq.com>
2023-07-29 23:08:54 +0800

    expression and function

---
+++ grammer/basic-types/src/01_expression.rs
@@ -0,0 +1,16 @@
+fn _define_statement() {
+    let a = 8;
+    let b: Vec<f64> = Vec::new();
+    let(a, c) = ("hi", false);
+
+    println!("a: {}, b: {}, c: {}", a,b,c);
+}
+
+fn _define_expression() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
+}
