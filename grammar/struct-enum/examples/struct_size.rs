ebc702f55bb3165119e85ee760789b5804a316b0
QuakeWang <1677980708@qq.com>
2023-08-25 11:49:19 +0800

    add size test of enum and struct

---
+++ grammar/struct-enum/examples/struct_size.rs
@@ -0,0 +1,17 @@
+use std::mem::{align_of, size_of};
+struct S1 {
+    a: u8,
+    b: u16,
+    c: u8,
+}
+
+struct S2 {
+    a: u8,
+    c: u8,
+    b: u16,
+}
+
+fn main() {
+    println!("sizeof S1: {}, S2: {}", size_of::<S1>(), size_of::<S2>());
+    println!("alignof S1: {}, S2: {}", align_of::<S1>(), align_of::<S2>());
+}
