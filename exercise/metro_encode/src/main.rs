de58f930f1f960350873eb8140f33b3c235e2df9
QuakeWang <1677980708@qq.com>
2023-09-27 14:56:18 +0800

    fun with metro_code

---
+++ exercise/metro_encode/src/main.rs
@@ -0,0 +1,23 @@
+use hex;
+use encoding::{Encoding, DecoderTrap};
+use encoding::all::GBK;
+
+fn main() {
+    let binary_codes = [
+        "1011101010111100",
+        "1101011011011101",
+        "1011010111011000",
+        "1100110011111010",
+    ];
+
+    let result: String = binary_codes
+        .iter()
+        .map(|x| {
+            let hex_string = format!("{:x}", u32::from_str_radix(x, 2).unwrap());
+            let bytes = hex::decode(hex_string).unwrap();
+            GBK.decode(&bytes, DecoderTrap::Strict).unwrap()
+        })
+        .collect();
+
+    println!("{}", result);
+}
