2fe7952c840ce7304a2fb14eccc0631b92547da8
QuakeWang <1677980708@qq.com>
2023-09-29 16:13:59 +0800

    add rc and refcell examples about dag

--- grammar/smart-pointers/src/borrow.rs
+++ grammar/smart-pointers/src/borrow.rs
@@ -5,7 +5,7 @@ fn main() {
 
     // 这里必须声明类型，因为 String 有多个 Borrow<T> 实现
     // 借用为 &String
-    let r1: &String = s.borrow();
+    let r1: &str = s.borrow();
     // 借用为 &str
     let r2: &str = s.borrow();
 
