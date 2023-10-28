db95796fad5b7ac681f3363d4520e50128678ca6
QuakeWang <1677980708@qq.com>
2023-10-28 23:39:08 +0800

    add some about network

---
+++ advanced/network/examples/rocket_server.rs
@@ -0,0 +1,23 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Hello {
+    name: String,
+}
+
+#[get("/", format = "json")]
+fn hello() -> Json<Hello> {
+    Json(Hello {
+        name: "Quake".into(),
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
+}
